a5bb62fbcc1091c34185a7193ed919b9f8f6358f
Donald Evans <donaldevans@Donalds-MacBook-Pro.local>
2018-09-11 22:23:20 -0600

    completed homework

---
+++ homework_part_2/js/app.js
@@ -0,0 +1,92 @@
+// PART 1:
+// 1. The difference between interpolation and
+// concatentation: Concatenation allows you to combine to strings 
+// together. It only works on strings.  
+// Example:  console.log("general " + "assembly");
+// Interpolation allows you to combine strings with variables .
+// Example:  const num = 5;
+// console.log(`they have ${num} pizzas`)
+
+
+// 2. DRY = Don't Repeat Yourself
+// Loops are very useful for writing DRY code.
+
+// 3. Declaration of a variable serves two purposes: 
+// It associates a type and an identifier (or name) with the variable.
+// Assignment is when we give a variable a specific value 
+// Defining a variable: Defining a variable means declaring it and 
+// also allocating space to hold it.
+
+// 4. Const should be used when we dont need to reassign a 
+// value to a variable
+// let should be used for variables that need to change values as 
+// a part of a later function
+
+// 5. A directory that contains other directories/files
+// 6. By using the help commands from within the command line
+// 7. Tab completion is an auto-complete feature that exists
+// in the command line.  It's awesome because it reduces the
+// amount of work needed to get around the computer using 
+// only the command line.
+
+// PART 2:
+ //const a = 4;
+ //const b = 53;
+ //const c = 57;
+ //const d = 16;
+ //const e = 'Kevin';
+
+ // 1. 
+ //console.log(a <= b);
+ // 2. 
+// console.log(c >= d);
+ // 3. 
+ //console.log('name' == 'name');
+ // 4. 
+ //console.log(a <= b <= c);
+ // 5. 
+ //console.log(a === a != d);
+ // 6. 
+ //console.log(e === 'Kevin');
+ // 7. 
+ //console.log(48 == '48');
+
+ //PART 3
+ // #1
+ // yes it's an infinite loop.  
+ // The condition will always be true and so it will run forever
+
+ // #2
+ // this is an infinite loop
+ // because the condition isn't broken despite being redfined as false
+
+// #3
+// 20 As should appear
+
+// PART 4
+
+// #1 while loops are better for when one is unsure of how long the
+// condition will allow the loop to run.  For loops have a definite 
+// number for looping the code.  They both repeat themselves usefully.
+
+// #2 
+// for(i = 0; i <= 999; i++){
+ //   console.log(i);
+
+//}
+
+// #3
+// * let i = 0;  this declares i and sets its value at 0
+// * i < 100; this sets a condition for the loop to execute as long as it is true
+// * i++ this incrementally increases i (by 1) for every successful loop that occurs
+
+// for(i = 999; i >= 0; i--){
+//    console.log(i);
+//}
+
+//for(i = 1; i <=10; i++){
+    //console.log(`The value of ${i} of 10`);
+//}
+
+
+
